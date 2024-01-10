5f51fdbe6074e3c5255352367402c410924f4d5c
didi9762 <yedidya9762@gmail.com>
2024-01-10 15:03:15 +0200

    check

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/client',ClientRouter)
 
+app.get('/',(req,res)=>{
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
